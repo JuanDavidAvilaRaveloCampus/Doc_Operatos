9d37688f81b875532ab4c58377dc21a6d8b258c7
JuanAvila856 <juandavidavila856@gmail.com>
2023-03-09 01:17:02 -0500

    feat: :memo: Logic Operator
    Logic operator described with tables and it's respective description

--- main.js
+++ main.js
@@ -1,37 +1,14 @@
 let operadores = {
     "NOMBRE" : "ABREVIACIÓN",
-    "Asignación": "x = y",
-    "Adición": "x += y",
-    "Sustracción": "x -= y",
-    "Multiplicación": "x *= y",
-    "División": "x /= y",
-    "Resto": "x %= y",
-    "Exponenciación": "x **= y",
-    "Desplazamiento a la izquierda": "x <<= y",
-    "Desplazamiento a la derecha": "x >>= y",
-    "Sin signo de desplazamiento": "x >>>= y",
-    "And": "x &= y",
-    "XOR": "x ^= y",
-    "Or": "x |= y"
+    "AND": " x && y",
+    "OR": "x | y",
+    "NOT": "!x"
 };
-
 let description = {
-    "NOMBRE" : "SIGNIFICADO",
-    "Asignación": "x = y",
-    "Adición": "x = x + y",
-    "Sustracción": "x = x - y",
-    "Multiplicación": "x = x * y",
-    "División": "x = x/y",
-    "Resto": "x = x % y",
-    "Exponenciación": "x = x ** y",
-    "Desplazamiento a la izquierda": "x = x << y",
-    "Desplazamiento a la derecha": "x = x >> y",
-    "Sin signo de desplazamiento": "x = x >>> y",
-    "And": "x = x & y",
-    "XOR": "x = x ^ y",
-    "Or": "x = x | y"
-};
-
+    "NOMBRE" : "DESCRIPCIÓN",
+    "AND": "Solo devuelve true si ambas condiciones son true, de otra forma será false",
+    "OR": "Devuelve true si una de las dos condiciones es true, o ambas lo son. En caso de no cumplir lo anterior, será false",
+    "NOT": "Devuele lo contrario: true = fase; false = true"
+}
 console.table(operadores)
-console.log("\n")
 console.table(description)
