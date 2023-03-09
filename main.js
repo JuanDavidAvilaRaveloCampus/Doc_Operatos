a5db43bb4026511ed37544d3d428d9cc888ff86a
JuanAvila856 <juandavidavila856@gmail.com>
2023-03-09 01:24:54 -0500

    feat: :memo: Atm Operator
    Table of arithmetic operators

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-let operadores = {
-    "NOMBRE" : "ABREVIACIÓN",
-    "AND": " x && y",
-    "OR": "x | y",
-    "NOT": "!x"
-};
-let description = {
-    "NOMBRE" : "DESCRIPCIÓN",
-    "AND": "Solo devuelve true si ambas condiciones son true, de otra forma será false",
-    "OR": "Devuelve true si una de las dos condiciones es true, o ambas lo son. En caso de no cumplir lo anterior, será false",
-    "NOT": "Devuele lo contrario: true = fase; false = true"
+let operator = {
+    "NOMBRE": "SYMBOL",
+    "Addition": "+",
+    "Decrement": "--",
+    "Division": "/",
+    "Exponentiation": "**",
+    "Increment": "++",
+    "Multiplication": "*",
+    "Remainer ": "%",
+    "Subtraction ": "-",
+    "Unary Negation ": "-",
+    "Unary Plus ": "+",
 }
-console.table(operadores)
-console.table(description)
+console.log(operator)
